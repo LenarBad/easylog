8d379ef15497701a3306eacff4f3d8d0c55161d9
lbadret <lbadret@rei.com>
2018-06-29 09:41:56 -0700

    cleanup

--- src/main/java/io/lenar/easy/log/EasyLoggerNoSpring.java
+++ src/main/java/io/lenar/easy/log/EasyLoggerNoSpring.java
@@ -9,18 +9,6 @@ public class EasyLoggerNoSpring extends UneasyLogger {
     public static final String CLASS_LEVEL_LOGIT_POINTCUT = "execution(* *(..)) && @within(annotation)";
     public static final String METHOD_LEVEL_LOGIT_POINTCUT = "execution(* *(..)) && @annotation(annotation)";
 
-//    @Pointcut("execution(* *(..)) && @within(annotation)")
-//    public void classLevelLogIt() {}
-//
-//    @Pointcut("execution(* *(..)) && @annotation(annotation)")
-//    public void methodLevelLogIt() {}
-//
-//    @Around("anyMethod() && @within(annotation)")
-//    public Object logItClassLevel(ProceedingJoinPoint jp, LogIt annotation) throws Throwable {
-//        return logMethod(jp, annotation);
-//    }
-//
-//    @Around("anyMethod() && @annotation(annotation)")
     public Object logIt(ProceedingJoinPoint jp, LogIt annotation) throws Throwable {
         return logMethod(jp, annotation);
     }
