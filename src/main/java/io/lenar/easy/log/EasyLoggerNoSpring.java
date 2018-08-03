c30797a7b941c55c1548d751411cc8ae38d73dae
lbadret <lbadret@rei.com>
2018-08-03 12:11:24 -0700

    update for non Spring logger

--- src/main/java/io/lenar/easy/log/EasyLoggerNoSpring.java
+++ src/main/java/io/lenar/easy/log/EasyLoggerNoSpring.java
@@ -3,6 +3,9 @@ package io.lenar.easy.log;
 import io.lenar.easy.log.annotations.LogIt;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+
+import static io.lenar.easy.log.support.PJPSupport.hasMethodLevelLogItAnnotation;
 
 public class EasyLoggerNoSpring extends UneasyLogger {
 
@@ -10,6 +13,17 @@ public class EasyLoggerNoSpring extends UneasyLogger {
     public static final String METHOD_LEVEL_LOGIT_POINTCUT = "execution(* *(..)) && @annotation(annotation)";
 
     public Object logIt(ProceedingJoinPoint jp, LogIt annotation) throws Throwable {
+        return logMethod(jp, annotation);
+    }
+
+    public Object logItClassLevel(ProceedingJoinPoint jp, LogIt annotation) throws Throwable {
+        if (hasMethodLevelLogItAnnotation(jp)) {
+            return jp.proceed(jp.getArgs());
+        }
+        return logMethod(jp, annotation);
+    }
+
+    public Object logItMethodLevel(ProceedingJoinPoint jp, LogIt annotation) throws Throwable {
         return logMethod(jp, annotation);
     }
 
