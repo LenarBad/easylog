d92f7b16308fd752a58be5d58e767d96eb792965
lbadret <lbadret@rei.com>
2018-08-08 09:21:41 -0700

    unit test for #42

--- src/test/java/io/lenar/easy/log/support/ProcessorTest.java
+++ src/test/java/io/lenar/easy/log/support/ProcessorTest.java
@@ -1,12 +1,13 @@
 package io.lenar.easy.log.support;
 
-import org.testng.Assert;
+import io.lenar.easy.log.support.testclasses.WithStaticFields;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
 
 public class ProcessorTest {
 
@@ -33,6 +34,16 @@ public class ProcessorTest {
 
         assertEquals(resultMap.size(), 2, "Expected to items in a Map");
         assertEquals(resultMap.get("2.2"), Processor.MASKED_VALUE, "Couldn't process Map<Long, String> and mask a field");
+    }
+
+    @Test
+    public void dontLogStaticField() {
+        WithStaticFields testObject = new WithStaticFields();
+        String[] maskFields = {"password"};
+        Map<String, String> resultMap = (Map<String, String>) Processor.process(testObject, maskFields);
+
+        assertFalse(resultMap.containsKey("staticField"), "staticFields has been logged");
+        assertEquals(resultMap.get("password"), Processor.MASKED_VALUE, "Field <password> should be masked");
     }
 
 }
